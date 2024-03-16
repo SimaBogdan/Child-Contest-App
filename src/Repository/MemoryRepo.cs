faad5f8dc469320379296141c5a7dc4a1298eabb
SimaBogdan <boogdansima095@yahoo.com>
2024-03-16 17:31:18 +0200

    I moved from Linux to Windows and this was just a test

--- src/Repository/MemoryRepo.cs
+++ src/Repository/MemoryRepo.cs
@@ -1,57 +1,56 @@
-using System;
-using System.Collections.Generic;
-using src.Model;
-
-
-namespace src.Model
-{
-    namespace src.Repository
-    {
-        public abstract class MemoryRepo<T> : IRepo<T>
-        {
-
-            protected List<T> entitati;
-
-            public MemoryRepo()
-            {
-                entitati = new List<T>();
-            }
-
-            public T getById(int id)
-            {
-                foreach (T entity in entitati)
-                {
-                    if (entity.id == id)
-                        return entity;
-                }
-                return null;
-
-            }
-
-            public void add(T e)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void update(T e)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void delete(int id)
-            {
-                throw new NotImplementedException();
-            }
-
-            public int size()
-            {
-                throw new NotImplementedException();
-            }
-
-            public List<T> getAll()
-            {
-                throw new NotImplementedException();
-            }
-        }
-    }
-}
+// using System;
+// using System.Collections.Generic;
+// using src.Model;
+//
+//
+// namespace src.Model
+// {
+//     namespace src.Repository
+//     {
+//         public abstract class MemoryRepo<T> 
+//         {
+//
+//             protected List<T> entitati;
+//
+//             public MemoryRepo()
+//             {
+//                 entitati = new List<T>();
+//             }
+//
+//             public T getById(int id)
+//             {
+//                 // foreach (T entity in entitati)
+//                 // {
+//                 //     if (entity.id == id)
+//                 //         return entity;
+//                 // }
+//                 
+//             }
+//
+//             public void add(T e)
+//             {
+//                 throw new NotImplementedException();
+//             }
+//
+//             public void update(T e)
+//             {
+//                 throw new NotImplementedException();
+//             }
+//
+//             public void delete(int id)
+//             {
+//                 throw new NotImplementedException();
+//             }
+//
+//             public int size()
+//             {
+//                 throw new NotImplementedException();
+//             }
+//
+//             public List<T> getAll()
+//             {
+//                 throw new NotImplementedException();
+//             }
+//         }
+//     }
+// }
--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf35d3d40e0001f735e40ad127b7a63da58c551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb88a11863769de1246b235da64a86c8ad16301")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
